cfb256b008b9a5de6f49a499d1be7aa5450611c6
ayrat <ayrat@nok2-test1.npopm.ru>
2017-01-11 17:38:53 +0700

    add first and second getting started projects

--- OfficialSpringExamples/src/main/java/chess715/study/Example.java
+++ OfficialSpringExamples/src/main/java/chess715/study/Example.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication//!
 public class Example {
 
     @RequestMapping("/")
