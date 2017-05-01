6cfa003c2618ccd518a3e72f19ec600451255c22
ajratb <ajratb@ya.ru>
2017-05-01 21:26:54 +0700

    Try to fix but filelogger does not write to file

--- Tkach/spring-tkach/src/main/java/ru/waytosky/spring/FileEventLogger.java
+++ Tkach/spring-tkach/src/main/java/ru/waytosky/spring/FileEventLogger.java
@@ -16,30 +16,38 @@ import org.apache.commons.io.FileUtils;
  *
  * @author Ayrat
  */
-public class FileEventLogger implements EventLogger{
+public class FileEventLogger implements EventLogger {
+
     String filename;
     private File file;
-    
-    public FileEventLogger(){
-        
+
+    public FileEventLogger() {
+
     }
 
     public FileEventLogger(String filename) {
+        System.out.println("FileLogger Constructor is acting");
         this.filename = filename;
     }
-    
-    
 
     @Override
     public void logEvent(Event event) {
         try {
-            FileUtils.writeStringToFile(file, event.toString(),
-                    "UTF-16", true);
+            System.out.println("yes, im trying");
+            FileUtils.writeStringToFile(file, "blablabla",//event.toString(),
+                    "UTF-16", false);
         } catch (IOException ex) {
+            System.out.println("failed");
             Logger.getLogger(FileEventLogger.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public void init() throws IOException{
-        this.file = new File(filename);
+
+    public void init() throws IOException {
+        file = new File(filename);
+        if (file.exists() && !file.canWrite()) {
+            throw new IllegalArgumentException("Can't write to file " + filename);
+        } else if (!file.exists()) {
+            file.createNewFile();
+        }
     }
 }
