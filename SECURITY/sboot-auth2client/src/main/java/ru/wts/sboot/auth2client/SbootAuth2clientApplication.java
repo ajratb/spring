b4f27f603c51da865c8ebcc94de9c90379f984cb
Ayrat Bik <ajratbik@gmail.com>
2020-08-27 22:59:05 +0700

    /SECURITY

---
+++ SECURITY/sboot-auth2client/src/main/java/ru/wts/sboot/auth2client/SbootAuth2clientApplication.java
@@ -0,0 +1,13 @@
+package ru.wts.sboot.auth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootAuth2clientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootAuth2clientApplication.class, args);
+	}
+
+}
