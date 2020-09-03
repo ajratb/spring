69164bbd151ebfd55f04064a5ba648176c06823e
Ayrat Bik <ajratbik@gmail.com>
2020-09-03 23:58:28 +0700

    sboot-oauth2client simple and ext projects

---
+++ SECURITY/baeldung/sboot-auth2client-simple/src/main/java/ru/wts/sboot/auth2client/SecurityConfig.java
@@ -0,0 +1,22 @@
+package ru.wts.sboot.auth2client;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@PropertySource("application-oauth2.properties")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+
+            .anyRequest()
+            .authenticated()
+            .and()
+            .oauth2Login();
+    }
+}
