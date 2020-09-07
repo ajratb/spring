bdcf2a200783b7c0395475f68d41635c63677e11
Ayrat Bik <ajratbik@gmail.com>
2020-09-07 23:52:37 +0700

    set in order security/baeldung projects

---
+++ SECURITY/baeldung/sboot-oauth2client-extractors/src/test/java/ru/wts/SbootAuthApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.wts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+            Assertions.assertThat(false).isEqualTo(false);
+	}
+
+}
