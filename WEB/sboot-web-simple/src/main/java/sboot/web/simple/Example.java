383f9ba0398884e9f93998468a7027390cc0c4e7
ayrat <ayrat@nok2-test1.npopm.ru>
2020-10-06 11:54:46 +0700

    update sboot-web-simple: home endpoint response

--- WEB/sboot-web-simple/src/main/java/sboot/web/simple/Example.java
+++ WEB/sboot-web-simple/src/main/java/sboot/web/simple/Example.java
@@ -10,7 +10,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "HTTP/1.1 200 OK\n";
     }
 
     public static void main(String[] args) {
