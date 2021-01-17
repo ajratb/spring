f1a3a589570d69686be30dfd9d0eb7a305c05746
Ayrat Bik <ajratbik@gmail.com>
2021-01-18 00:01:01 +0700

    update spring start-app

--- SPRING/start-app/src/main/java/wts/props/AppWithProps.java
+++ SPRING/start-app/src/main/java/wts/props/AppWithProps.java
@@ -1,15 +1,26 @@
 package wts.props;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Repository;
 
-@Repository//to put instance in context
+@Component // to put instance in context. You also can use @Service, @Repository
+@PropertySource("classpath:app.properties")//for using @Value
 public class AppWithProps {
 
 	@Autowired
 	Message msg;
+
+	private final String value;
+
+	public AppWithProps(@Value("${my_value}") String value) {//Use @Value with @PropertySource
+		super();
+		this.value = value;
+	}
 
 	public static void main(String[] args) {
 
@@ -21,6 +32,8 @@ public class AppWithProps {
 
 			Message msgBean = ctx.getBean(Message.class);
 			msgBean.printMsg();
+
+			System.out.println("Value is: " + app.value);
 		}
 	}
 }
