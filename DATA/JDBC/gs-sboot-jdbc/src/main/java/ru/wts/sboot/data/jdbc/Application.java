9b3f3082f0a22cb4cccb49e41ecb068362b94d30
ayrat <ayrat@nok2-test1.npopm.ru>
2021-07-23 16:20:46 +0700

    spring data maps updated

--- DATA/JDBC/gs-sboot-jdbc/src/main/java/ru/wts/sboot/data/jdbc/Application.java
+++ DATA/JDBC/gs-sboot-jdbc/src/main/java/ru/wts/sboot/data/jdbc/Application.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
-//    @Autowired
-//    ApplicationRunner appRunner;
     
     public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
