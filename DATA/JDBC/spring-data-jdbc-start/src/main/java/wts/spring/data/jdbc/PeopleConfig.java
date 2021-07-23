1dd466ef91ae238bf98237f35d6d298ddb13e799
ayrat <ayrat@nok2-test1.npopm.ru>
2021-07-23 16:21:21 +0700

    spring-data-jdbc-start works with h2

--- DATA/JDBC/spring-data-jdbc-start/src/main/java/wts/spring/data/jdbc/PeopleConfig.java
+++ DATA/JDBC/spring-data-jdbc-start/src/main/java/wts/spring/data/jdbc/PeopleConfig.java
@@ -1,65 +1,67 @@
 package wts.spring.data.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
-//import org.springframework.data.jdbc.repository.config.JdbcRepositoryConfigExtension;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("wts.spring.data.jdbc")
 @EnableJdbcRepositories
 @EnableTransactionManagement // is it needed. Can i perform transactions without it?
-@PropertySource("classpath:postgres-dev.properties")
+//@PropertySource("classpath:postgres-dev.properties")
 public class PeopleConfig extends AbstractJdbcConfiguration { // that's from documentation
 
 	// JdbcConfiguration - it seems to be old class(from habr 2018)
 
-	@Autowired
-	Environment environment;
-	private final String URL = "url";
-	private final String USER = "dbuser";
-	private final String DRIVER = "driver";
-	private final String PASSWORD = "dbpassword";
+//	@Autowired
+//	Environment environment;
+//	private final String URL = "url";
+//	private final String USER = "dbuser";
+//	private final String DRIVER = "driver";
+//	private final String PASSWORD = "dbpassword";
 
 	@Bean
 	PlatformTransactionManager transactionManager() {
-		return new DataSourceTransactionManager(pgDataSource());
-	}
-
-	@Bean
-	public DataSource pgDataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(environment.getProperty(DRIVER));
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/devdb");
-		dataSource.setUsername("dev");
-		// CHECK IF IT DEPENDS ON pg_hba.conf
-		dataSource.setPassword(environment.getProperty(PASSWORD));
-		return dataSource;
+		return new DataSourceTransactionManager(dataSource());
 	}
 
 	@Bean
 	NamedParameterJdbcOperations operations() {
-		return new NamedParameterJdbcTemplate(pgDataSource());
+		return new NamedParameterJdbcTemplate(dataSource());
 	}
 
-//    @Bean
-//    DataSource dataSource(){
-//        return new EmbeddedDatabaseBuilder()
+    @Bean
+    DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
 //                .generateUniqueName(true)
-//                .setType(EmbeddedDatabaseType.HSQL)
+                .setType(EmbeddedDatabaseType.H2)
 //                .addScript("create-customer-schema.sql")
-//                .build();
-//    }
+				.addScript("db.sql")
+                .build();
+    }
+
+//	@Bean
+//	public DataSource pgDataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName(environment.getProperty(DRIVER));
+//		dataSource.setUrl("jdbc:postgresql://localhost:5432/devdb");
+//		dataSource.setUsername("dev");
+//		// CHECK IF IT DEPENDS ON pg_hba.conf
+//		dataSource.setPassword(environment.getProperty(PASSWORD));
+//		return dataSource;
+//	}
 }
---
+++ DATA/JDBC/spring-data-jdbc-start/src/main/resources/db.sql
@@ -0,0 +1,20 @@
+--<ScriptOptions statementTerminator=";"/>
+
+--ALTER TABLE courses DROP CONSTRAINT courses_pkey IF EXISTS;
+
+DROP TABLE people IF EXISTS;
+
+CREATE TABLE people (
+		id BIGINT auto_increment,
+		age INT,
+		first_name TEXT(2147483647),
+		last_name TEXT(2147483647)
+	);
+
+--ALTER TABLE courses ADD CONSTRAINT courses_pkey PRIMARY KEY (c_no);
+--insert into courses(c_no, title, hours) values('005','Math', 245);
+--insert into courses(c_no, title, hours) values('006','Algebra', 145);
+--insert into courses(c_no, title, hours) values('007','Chemistry', 45);
+--insert into courses(c_no, title, hours) values('010','IT', 154);
+
+
