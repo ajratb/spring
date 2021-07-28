9d309e6d132a66032d0c1fac6d03e4c1e7047433
ayrat <ayrat@nok2-test1.npopm.ru>
2021-07-28 10:42:54 +0700

    update spring-data-jdbc

--- DATA/JDBC/spring-data-jdbc-start/src/main/java/wts/spring/data/jdbc/PeopleRepository.java
+++ DATA/JDBC/spring-data-jdbc-start/src/main/java/wts/spring/data/jdbc/PeopleRepository.java
@@ -3,7 +3,7 @@ package wts.spring.data.jdbc;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
 interface PeopleRepository extends CrudRepository<People, Long> {
 
 }
