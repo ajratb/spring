6c50a122d9286142a9490451a3a1c9c300cf04cd
ayrat <ajratb@ya.ru>
2022-09-09 11:18:51 +0700

    data-rest-demo

--- WEB/REST/data-rest-demo/src/test/java/ru/wts/datarestdemo/DataRestDemoApplicationTests.java
+++ WEB/REST/data-rest-demo/src/test/java/ru/wts/datarestdemo/DataRestDemoApplicationTests.java
@@ -1,13 +1,18 @@
 package ru.wts.datarestdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DataRestDemoApplicationTests {
 
+	@Autowired PersonRepository personRepository;
 	@Test
 	void contextLoads() {
+		assertThat(personRepository).isNotNull();
 	}
 
 }
