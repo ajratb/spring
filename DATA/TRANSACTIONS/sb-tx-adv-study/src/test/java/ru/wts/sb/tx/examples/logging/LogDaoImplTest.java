173815cd975c617f6c25a0555386bc7b9152b801
ayrat <ajratb@ya.ru>
2023-03-28 22:19:53 +0700

    Remove method to LogDaoImplTest

--- DATA/TRANSACTIONS/sb-tx-adv-study/src/test/java/ru/wts/sb/tx/examples/logging/LogDaoImplTest.java
+++ DATA/TRANSACTIONS/sb-tx-adv-study/src/test/java/ru/wts/sb/tx/examples/logging/LogDaoImplTest.java
@@ -1,6 +1,7 @@
 package ru.wts.sb.tx.examples.logging;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,5 +16,17 @@ class LogDaoImplTest {
     @BeforeEach
     void setUp() {
         logDao.deleteAll();
+    }
+
+    @Test
+    public void supports() {
+        // executing without transaction:
+        // addSeparateLogsSupports is working with no transaction
+        try {
+            //logDao.addSeparateLogsSupports();
+            logDao.addSeparateLogsSupportsWithTransaction();
+        } catch (Exception e) {}
+        // no transaction - first record is added in the log even after exception
+        logDao.showLogs();
     }
 }
