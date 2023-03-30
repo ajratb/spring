9937606d46869ef5bf1178de3d66f50ea72bfc25
ayrat <ajratb@ya.ru>
2023-03-30 17:56:58 +0700

    rename sb-tx-adv-course

--- DATA/TRANSACTIONS/sb-tx-db/src/main/java/wts/sb/tx/db/dao/book/exception/DuplicateBookTitleException.java
+++
@@ -1,8 +0,0 @@
-package wts.sb.tx.db.dao.book.exception;
-
-public class DuplicateBookTitleException extends RuntimeException {
-
-	public DuplicateBookTitleException(String message) {
-		super(message);
-	}
-}
--- DATA/TRANSACTIONS/sb-tx-db/src/main/java/wts/sb/tx/db/dao/logging/LogDao.java
+++
@@ -1,6 +0,0 @@
-package wts.sb.tx.db.dao.logging;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wts.sb.tx.db.entities.Log;
-
-public interface LogDao extends JpaRepository<Log, Integer> { }
