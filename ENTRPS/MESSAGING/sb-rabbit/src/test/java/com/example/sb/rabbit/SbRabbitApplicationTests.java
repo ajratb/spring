77f84cb6cc5df8f509bfa0f05466adb25e96649d
ayrat <ajratb@ya.ru>
2023-04-06 11:56:25 +0700

    Create sb-rabbit

---
+++ ENTRPS/MESSAGING/sb-rabbit/src/test/java/com/example/sb/rabbit/SbRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb.rabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
