5769f651721ebda4371b57ccd9e8ba1e18c69a33
ayrat <ajratb@ya.ru>
2023-04-06 14:25:09 +0700

    Complete by gs guide

---
+++ ENTRPS/MESSAGING/sb-rabbit/src/main/java/com/example/sb/rabbit/Runner.java
@@ -0,0 +1,29 @@
+package com.example.sb.rabbit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("unused")
+@Component
+@Slf4j
+public class Runner implements CommandLineRunner {
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+        this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(SbRabbitApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+        boolean awaitResult = receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        log.info("Runner finished successful: {}", awaitResult);
+    }
+}
