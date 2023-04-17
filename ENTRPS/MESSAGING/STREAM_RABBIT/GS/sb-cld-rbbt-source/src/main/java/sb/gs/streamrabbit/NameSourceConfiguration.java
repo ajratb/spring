f26943834a8c6c600b0a917d11cac81348468267
ayrat <ajratb@ya.ru>
2023-04-17 16:34:01 +0700

    Complete project by example

---
+++ ENTRPS/MESSAGING/STREAM_RABBIT/GS/sb-cld-rbbt-source/src/main/java/sb/gs/streamrabbit/NameSourceConfiguration.java
@@ -0,0 +1,17 @@
+package sb.gs.streamrabbit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Supplier;
+
+@Configuration
+public class NameSourceConfiguration {
+
+	// tag::supplyname[]
+	@Bean
+	public Supplier<String> supplyName() {
+		return () -> "Christopher Pike";
+	}
+	// end::supplyname[]
+}
