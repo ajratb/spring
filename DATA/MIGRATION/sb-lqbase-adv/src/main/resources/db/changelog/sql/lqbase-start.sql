bb303e47f2e8b1628b1563950912c62cd73799f7
ayrat <ajratb@ya.ru>
2023-07-29 22:31:42 +0700

    Usage different types of changelogs

---
+++ DATA/MIGRATION/sb-lqbase-adv/src/main/resources/db/changelog/sql/lqbase-start.sql
@@ -0,0 +1,24 @@
+-- Get Started with Liquibase and SQL
+-- https://docs.liquibase.com/start/get-started/liquibase-sql.html
+
+--changeset your.name:1
+create table person
+(
+    id       int primary key,
+    name     varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city     varchar(30)
+);
+--changeset your.name:2
+create table company
+(
+    id       int primary key,
+    name     varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city     varchar(30)
+);
+--changeset other.dev:3
+alter table person
+    add column country varchar(2)
