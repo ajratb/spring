817125922cebd082f52879e7f2c12affbc5bbadb
ayrat <ajratb@ya.ru>
2023-09-13 19:12:57 +0700

    init project

---
+++ WEB/CLIENTS/sb-webclient/src/main/java/sb/webclient/SbWebclientApplication.java
@@ -0,0 +1,13 @@
+package sb.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWebclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbWebclientApplication.class, args);
+    }
+
+}
---
+++ WEB/CLIENTS/sb-webclient/src/test/java/sb/webclient/SbWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbWebclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
