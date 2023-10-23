b98866107ca8bb8f3aac6bf428308fac3ee05a78
Бикчентаев Айрат <a.bikchentaev@grfc.ru>
2023-10-23 22:17:26 +0700

    WebClient Examples

--- WEB/CLIENTS/sb-webclient/src/main/java/sb/webclient/SbWebclientApplication.java
+++ WEB/CLIENTS/sb-webclient/src/main/java/sb/webclient/SbWebclientApplication.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
@@ -23,28 +26,40 @@ public class SbWebclientApplication implements CommandLineRunner {
             log.info("Class: {}", ex.getClass());
             log.info("Message: {}", ex.getMessage());
         }
-        try {
-            WebClient.create("https://yaRRRR.ru/34234").get().retrieve().bodyToMono(String.class).block();
-        } catch (Exception ex) {
-            log.info("=== EXCEPTION ===");
-            log.info("Class: {}", ex.getClass());
-            log.info("Message: {}", ex.getMessage());
-        }
+//        try {
+//            WebClient.create("https://yaRRRR.ru/34234").get().retrieve().bodyToMono(String.class).block();
+//        } catch (Exception ex) {
+//            log.info("=== EXCEPTION ===");
+//            log.info("Class: {}", ex.getClass());
+//            log.info("Message: {}", ex.getMessage());
+//        }
 
         HttpClient httpClient = HttpClient.create()
                 .responseTimeout(Duration.ofSeconds(4));
         WebClient webClient = WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
-        try {
-            String result = webClient.get().uri("http://localhost:8080/creatives/sleep")
-                    .retrieve().bodyToMono(String.class).block();
-            log.info("Result is: {}", result);
-        } catch (Exception ex) {
-            log.info("=== EXCEPTION ===");
-            log.info("Class: {}", ex.getClass());
-            log.info("Message: {}", ex.getMessage());
-        }
+//        try {
+//            String result = webClient.get().uri("http://localhost:8080/creatives/sleep")
+//                    .retrieve().bodyToMono(String.class).block();
+//            log.info("Result is: {}", result);
+//        } catch (Exception ex) {
+//            log.info("=== EXCEPTION ===");
+//            log.info("Class: {}", ex.getClass());
+//            log.info("Message: {}", ex.getMessage());
+//        }
+
+        log.info("START");
+        Mono<ResponseEntity<Void>> reponse = webClient.get()
+                .uri("https://ord.ozon.ru/api/ord-api/content/media/bf426fb1463ec1e857a6d238a5b7099c68a7862d3e0c468726edf141a5496eb6")
+                //.header("Content-Length", "1000")
+                .retrieve().toBodilessEntity()
+                .doOnSuccess(entity -> log.info("clientResponse.headers() = {}",entity.getHeaders()))
+                .doOnSuccess(entity -> log.info("clientResponse.statusCode() = {}",entity.getStatusCode()));
+//                .flatMap(clientResponse -> Mono.just("DONE!"));//clientResponse.bodyToMono(String.class));
+
+        ResponseEntity<Void> block = reponse.block();
+        log.info("DONE");
     }
 
     public static void main(String[] args) {
