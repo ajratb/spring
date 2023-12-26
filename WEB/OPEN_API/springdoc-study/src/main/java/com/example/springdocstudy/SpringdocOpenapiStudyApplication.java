b6b2fcf5ee3f833249462882f8f8bfc946399194
Бикчентаев Айрат <a.bikchentaev@grfc.ru>
2023-12-26 12:47:12 +0700

    springdoc-study

---
+++ WEB/OPEN_API/springdoc-study/src/main/java/com/example/springdocstudy/SpringdocOpenapiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdocstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocOpenapiStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocOpenapiStudyApplication.class, args);
+    }
+
+}
---
+++ WEB/OPEN_API/springdoc-study/src/test/java/com/example/springdocstudy/SpringdocOpenapiStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdocstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocOpenapiStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
