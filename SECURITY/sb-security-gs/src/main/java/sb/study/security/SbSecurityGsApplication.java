dc809212e26b547758aca44b193196bb6c9fdbb5
Бикчентаев Айрат <a.bikchentaev@grfc.ru>
2023-12-27 00:04:04 +0700

    add sb-security-gs

---
+++ SECURITY/sb-security-gs/src/main/java/sb/study/security/SbSecurityGsApplication.java
@@ -0,0 +1,13 @@
+package sb.study.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityGsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityGsApplication.class, args);
+	}
+
+}
---
+++ SECURITY/sb-security-gs/src/test/java/sb/study/security/SbSecurityGsApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.study.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurityGsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
