2912028062c0d8b39fd01426f839800576b6f1b4
Бикчентаев Айрат <a.bikchentaev@grfc.ru>
2024-01-15 21:15:27 +0700

    Create separate rabbit projects

---
+++ ENTRPS/MESSAGING/sb-rabbit-consumer/src/main/java/com/example/sb/rabbit/Runner.java
@@ -0,0 +1,41 @@
+package com.example.sb.rabbit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("unused")
+@Component
+@Slf4j
+public class Runner implements CommandLineRunner {
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+
+    @Value("${rabbitmq.topic}") String topic;
+
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+        this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    /**
+     * Run application.
+     * If you will use ..amqp.core.Message then use Receiver::receiveBytes
+     * (with byte[] as parameter type, not amqp.core.Message nor Object nor String)
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Sending message...");
+        // change string-message by message-from-builder
+//        rabbitTemplate.convertAndSend(topic, "foo.bar.baz", org.springframework.amqp.core.MessageBuilder.withBody("Hello from RabbitMQ!".getBytes()).build());
+        rabbitTemplate.convertAndSend(topic, "foo.bar.baz", MessageBuilder.withPayload("Hello from RabbitMQ!").build());
+//        rabbitTemplate.convertAndSend(topic, "foo.bar.baz", "Hello from RabbitMQ!");
+        boolean awaitResult = receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        log.info("Runner finished successful: {}", awaitResult);
+    }
+}
---
+++ ENTRPS/MESSAGING/sb-rabbit-producer/src/test/java/com/example/sb/rabbit/SbRabbitApplicationTests.java
@@ -0,0 +1,58 @@
+package com.example.sb.rabbit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.AmqpConnectException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.concurrent.TimeUnit;
+
+/*
+ * Uncompleted code from official github.
+ * Almost the same as in Runner.
+ * https://github.com/spring-guides/gs-messaging-rabbitmq/blob/main/complete/src/test/java/com/example/messagingrabbitmq/MessagingRabbitmqApplicationTest.java
+ */
+@SpringBootTest
+@Slf4j
+class SbRabbitApplicationTests {
+
+	@SuppressWarnings("unused")
+	@MockBean
+	private Runner runner;
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	@Autowired
+	private Receiver receiver;
+
+	@Value("${rabbitmq.queue}") String queue;
+
+	/*
+	 * The same as in Runner.
+	 * But in convertAndSend set queue instead of topic
+	 * No assertions yet.
+	 */
+	@Test
+	public void test() throws Exception {
+		try {
+			System.out.println("Sending message in queue...");
+			// here queue is being used
+			rabbitTemplate.convertAndSend(queue, "Hello from RabbitMQ!");
+			boolean awaitResult = receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+			System.out.println("Runner finished successful: " + awaitResult);
+			log.info("Runner finished successful: {}", awaitResult);
+		} catch (AmqpConnectException e) {
+			// ignore - rabbit is not running
+		}
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
