985ed02cc8cfd4ae0976f7537d37c9220baa5b84
Бикчентаев Айрат <a.bikchentaev@grfc.ru>
2024-01-29 15:33:25 +0700

    sboot-mapstruct-study init

---
+++ DATA/MAPPING/sboot-mapstruct-study/src/main/java/sboot/study/mapstruct/SbootMapstructApplication.java
@@ -0,0 +1,13 @@
+package sboot.study.mapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootMapstructApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbootMapstructApplication.class, args);
+    }
+
+}
