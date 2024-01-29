33fc5e42b565f0a3ac450ad8edd7fc2b40d495a0
Бикчентаев Айрат <a.bikchentaev@grfc.ru>
2024-01-29 15:33:25 +0700

    sboot-mapstruct-study init

---
+++ DATA/MAPPING/sboot-mapstruct-study/src/main/java/sboot/study/mapstruct/example2/Supplier.java
@@ -0,0 +1,12 @@
+package sboot.study.mapstruct.example2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Supplier {
+
+    private Long id;
+    private String name;
+}
---
+++ DATA/MAPPING/sboot-mapstruct-study/src/main/java/sboot/study/mapstruct/example2/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This example is from here:
+ * https://bootify.io/spring-data/mapstruct-with-maven-and-lombok.html
+ * (Using MapStruct with Maven and Lombok)
+ */
+package sboot.study.mapstruct.example2;
