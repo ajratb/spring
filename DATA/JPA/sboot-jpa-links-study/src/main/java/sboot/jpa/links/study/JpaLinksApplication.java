9200cd5448164a51979e8aa26f0e9150591a5aa1
Бикчентаев Айрат <a.bikchentaev@grfc.ru>
2024-02-12 22:32:36 +0700

    sboot-jpa-links-study: employee entity

--- DATA/JPA/sboot-jpa-links-study/src/main/java/sboot/jpa/links/study/JpaLinksApplication.java
+++ DATA/JPA/sboot-jpa-links-study/src/main/java/sboot/jpa/links/study/JpaLinksApplication.java
@@ -1,13 +1,30 @@
 package sboot.jpa.links.study;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sboot.jpa.links.study.entity.Employee;
+import sboot.jpa.links.study.repository.EmployeeRepository;
 
+import java.util.List;
+
+@Slf4j
 @SpringBootApplication
-public class JpaLinksApplication {
+public class JpaLinksApplication implements CommandLineRunner {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employee johny = employeeRepository.save(new Employee("Johny"));
+		List<Employee> all = employeeRepository.findAll();
+		all.forEach(emp -> System.out.println("Employee id: " + emp.getId() + ", name: " + emp.getName()));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaLinksApplication.class, args);
 	}
-
 }
--- DATA/JPA/sboot-jpa-links-study/src/main/java/sboot/jpa/links/study/entity/Employee.java
+++ DATA/JPA/sboot-jpa-links-study/src/main/java/sboot/jpa/links/study/entity/Employee.java
@@ -1,11 +1,23 @@
 package sboot.jpa.links.study.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Getter
 @Entity
 public class Employee {
 
     @Id
+    @GeneratedValue
     long id;
+
+    String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
 }
